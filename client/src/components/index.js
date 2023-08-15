11f312924c5f084af1f0ae1c9c19b88470a319d6
devanshk404 <zazen4745@gmail.com>
2023-08-16 00:00:52 +0530

    basic done

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export {defalut as Loader } from './Loader';
+export {defalut as Navbar } from './Navbar';
+export {defalut as Footer } from './Footer';
+export {defalut as Services } from './Services';
+export {defalut as Transactions } from './Transactions';
+export {defalut as Welcome } from './Welcome';
